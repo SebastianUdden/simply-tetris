6d68298bd5b16b41b0fb9274cf82314412b2a09c
Sebastian Uddén <sebastian.udden@nobia.com>
2020-08-31 18:39:19 +0200

    feat: view placement and improve UI

--- src/components/EnterHighScore.js
+++ src/components/EnterHighScore.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
 const Form = styled.div`
@@ -7,7 +7,7 @@ const Form = styled.div`
   align-items: center;
 `
 const HighScore = styled.p`
-  font-size: 30px;
+  font-size: 25px;
   width: 100%;
   display: flex;
   justify-content: space-between;
@@ -24,43 +24,84 @@ const Input = styled.input`
   padding: 10px;
   width: 100%;
   max-width: 95%;
+  margin-bottom: 10px;
+`
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 10px;
 `
 
 const Button = styled.button`
-  margin: 20px 0;
-  padding: 1rem 3rem;
+  :first-child {
+    margin-right: 10px;
+  }
+  padding: 15px;
   width: 100%;
   font-size: large;
   border: none;
-  background-color: #444;
+  background-color: ${p => (p.disabled ? "#aaa" : "#444")};
   color: white;
   :hover {
-    cursor: pointer;
-    background-color: #777;
+    cursor: ${p => (p.disabled ? "not-allowed" : "pointer")};
+    background-color: ${p => (p.disabled ? "#aaa" : "#777")};
   }
 `
 
+const getEnding = place => {
+  if (place === 1) return "st"
+  if (place === 2) return "nd"
+  if (place === 3) return "rd"
+  return "th"
+}
+
+const getPlacementText = place => {
+  if (place === -1) return "You placed last: "
+  const placement = place + 1
+  return `You placed ${placement}${getEnding(placement)}:`
+}
+
 export default ({ highScores, score, onSubmit }) => {
-  const [value, setValue] = useState("")
+  const [name, setName] = useState("")
+  const [placement, setPlacement] = useState(1)
+  useEffect(() => {
+    const place = highScores.findIndex(highScore => highScore.score < score)
+    setPlacement(getPlacementText(place))
+  }, [])
+
   return (
     <Form>
       <HighScore>
-        <Name>You score: </Name>
+        <Name>{placement}</Name>
         <Score>{score}</Score>
       </HighScore>
-      <Input onChange={e => setValue(e.target.value)} value={value} />
-      <Button
-        onClick={() => {
-          const newObj = { name: value, score }
-          const newHighScores = [...highScores, newObj].sort(
-            (a, b) => b.score - a.score
-          )
+      <Input
+        id="username"
+        placeholder="Enter username"
+        onChange={e => setName(e.target.value)}
+        value={name}
+      />
+      <Buttons>
+        <Button
+          onClick={() => {
+            if (!name) {
+              document.getElementById("username").focus()
+              return
+            }
+            const newObj = { name, score }
+            const newHighScores = [...highScores, newObj].sort(
+              (a, b) => b.score - a.score
+            )
 
-          onSubmit(value ? newHighScores : highScores)
-        }}
-      >
-        Submit
-      </Button>
+            onSubmit(name ? newHighScores : highScores)
+          }}
+        >
+          Submit
+        </Button>
+        <Button onClick={() => onSubmit(highScores)}>Cancel</Button>
+      </Buttons>
     </Form>
   )
 }
